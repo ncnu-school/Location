31f21b6f93fdfdfac7824d3f95a9787cf147f285
LAPTOP-461AOIA4\User <victor920617@gmail.com>
2023-11-06 16:08:36 +0800

    first commit

---
+++ Location/Location/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Location
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void GetAndDisplayLocation(object sender,EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                {
+                    DesiredAccuracy = GeolocationAccuracy.Best,
+                    Timeout = TimeSpan.FromSeconds(30)
+                });
+
+                if (location != null)
+                {
+                    locationLabel.Text = $"Location: 緯度Latitude {location.Latitude}, 經度Longitude {location.Longitude}";
+                }
+                else
+                {
+                    locationLabel.Text = "Unable to get location";
+                }
+            }
+            catch (Exception ex)
+            {
+                // 处理异常
+                locationLabel.Text = $"Error: {ex.Message}";
+            }
+        }
+    }
+}
